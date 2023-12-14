85b726e4465109db4f735ef7871252b0841af0ae
Ashkan1373 <ashkan1373@hotmail.com>
2023-12-14 16:37:50 -0500

    Adding the RMarkdown file and pre made pdf report

--- instructions.R
+++ instructions.R
@@ -1,10 +1,5 @@
-setwd(getwd())
-library(tidyverse)
+#Loading libraries
+source("scripts/libraries.R")
 
-data1 <- read_csv("source/CVD_cleaned.csv")
-
-figure1 <- ggplot(data1) + geom_boxplot(aes(x=FriedPotato_Consumption))
-figure2 <- ggplot(data1) + geom_histogram(aes(x=BMI))
-
-ggsave(filename = "figures/figure1.png", plot = figure1)
-ggsave(filename = "figures/figure2.png", plot = figure2)
+#making pdf
+render("~/work/report.Rmd", output_file = "report.pdf")
